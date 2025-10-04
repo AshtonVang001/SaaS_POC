567e6d36ec70f0749ba0a6d0f69436e30ee14f78
Ashton Vang <ashtonvang100@gmail.com>
2025-10-04 13:23:22 -0700

    feat: add appointments input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Appointments } from './pages/appointments'
--- frontend/src/pages/appointments.tsx
+++ frontend/src/pages/appointments.tsx
@@ -1,5 +1,10 @@
 export const  Appointments = () => {
     return (
+        <div>
         <div>appointments</div>
+        <input placeholder="Company Name"></input>
+        <input placeholder="Email"></input>
+        <input placeholder="Password"></input>
+        </div>
     )
 }
