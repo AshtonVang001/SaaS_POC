ec0f377bded544306d1cff61d4b9d49af04e7751
Ashton Vang <ashtonvang100@gmail.com>
2025-10-04 11:00:13 -0700

    feat: add appointment button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,10 @@ import './App.css'
 function App() {
 
   return (
-    <div>Hello</div>
+    <div>
+      Welcome to SaaS how can i help you
+      <button>appointments</button>
+    </div>
   )
 }
 
