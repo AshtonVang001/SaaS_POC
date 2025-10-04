575a0266a38ad8b8973319bdc01b4883e22f678a
Ashton Vang <ashtonvang100@gmail.com>
2025-10-04 13:43:08 -0700

    feat: add submite button

--- frontend/src/pages/appointments.tsx
+++ frontend/src/pages/appointments.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
+
 export const  Appointments = () => {
+    const [inputCompany, getCompany] = useState("");
+    const [inputEmail, getEmail] = useState("");
+    const [inputPassword, getPassowrd] = useState("");
+
+
+
+
+
+
     return (
         <div>
         <div>appointments</div>
-        <input placeholder="Company Name"></input>
-        <input placeholder="Email"></input>
-        <input placeholder="Password"></input>
+        <input placeholder="Company Name" type="text"></input>
+        <input placeholder="Email" type="text"></input>
+        <input placeholder="Password" type="text"></input>
+        <button>Submit</button>
         </div>
     )
 }
+
+//GET is sending data from the backend to the frontend
+//POST is sending data from the frontend to the backend
