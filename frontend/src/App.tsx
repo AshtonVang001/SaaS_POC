ee4c1c25a45b4ad79f3cd1753912dadca668eed4
Ashton Vang <ashtonvang100@gmail.com>
2025-10-06 16:17:55 -0700

    feat: export app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,3 +17,14 @@ function App() {
 }
 
 export default App
+
+
+//right now appointments page is the actual sign up for appointments
+//make a page to where they can actually book appointments
+//have users be able to book appointments based on company
+
+//flow:
+//company wants to have a appoinment online service
+//they sign up (implement payment later)
+//they get their own appointments page
+//users can go to an appointments page, pick the company then make the appointment
--- frontend/src/pages/appointments.tsx
+++ frontend/src/pages/appointments.tsx
@@ -1,25 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const  Appointments = () => {
-    const [inputCompany, getCompany] = useState("");
-    const [inputEmail, getEmail] = useState("");
-    const [inputPassword, getPassowrd] = useState("");
+//company sign up
+export const Appointments = () => {
+  const [inputCompany, getCompany] = useState("");
+  const [inputEmail, getEmail] = useState("");
+  const [inputPassword, getPassowrd] = useState("");
 
+  const handleSubmit = async () => {
+    const res = await fetch("http://localhost:4000/api/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        companyInput: inputCompany,
+        emailIput: inputEmail,
+        passwordInput: inputPassword,
+      }),
+    });
 
+    const data = await res.json();
+    console.log("Response from backend", data);
+  };
 
-
-
-
-    return (
-        <div>
-        <div>appointments</div>
-        <input placeholder="Company Name" type="text"></input>
-        <input placeholder="Email" type="text"></input>
-        <input placeholder="Password" type="text"></input>
-        <button>Submit</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>appointments</div>
+      <input
+        placeholder="Company Name"
+        type="text"
+        value={inputCompany}
+        onChange={(e) => getCompany(e.target.value)}
+      ></input>
+      <input
+        placeholder="Email"
+        type="text"
+        value={inputEmail}
+        onChange={(e) => getEmail(e.target.value)}
+      ></input>
+      <input 
+      placeholder="Password" 
+      type="text"
+      value={inputPassword}
+      onChange={(e) => getPassowrd(e.target.value)}
+      ></input>
+      <button>Submit</button>
+    </div>
+  );
+};
 
 //GET is sending data from the backend to the frontend
 //POST is sending data from the frontend to the backend
