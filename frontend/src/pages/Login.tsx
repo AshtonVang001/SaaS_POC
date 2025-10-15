b8f2f42fbc69d437d7881f15cd0de56a8e18bfb8
Ashton Vang <ashtonvang100@gmail.com>
2025-10-14 22:36:37 -0700

    feat: add components to login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+  const [inputCompany, getCompany] = useState("");
+  const [inputEmail, getEmail] = useState("");
+  const [inputPassword, getPassoword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  const handleSubmit = async () => {};
+
+  return (
+    <div>
+      <input
+        placeholder="Company Name"
+        type="text"
+        value={inputCompany}
+        onChange={(e) => getCompany(e.target.value)}
+      ></input>
+      <input
+        placeholder="Email"
+        type="text"
+        value={inputEmail}
+        onChange={(e) => getEmail(e.target.value)}
+      ></input>
+      <input
+        placeholder="Password"
+        type="text"
+        value={inputPassword}
+        onChange={(e) => getPassoword(e.target.value)}
+      ></input>
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleBack}>back</button>
+    </div>
+  );
+};
